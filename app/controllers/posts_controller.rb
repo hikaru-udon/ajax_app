6b77f57a039eb1d772152916809fb5e67581f1f7
hikaru-udon <mare.nozomu512@gmail.com>
2020-11-19 11:48:55 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content], checked: false)
-    render json:{ post: post}
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
+    # redirect_to action: :index
   end
 
   def checked
     #binding.pry
     post = Post.find(params[:id])
-    if post.checked 
+    if post.checked
       post.update(checked: false)
     else
       post.update(checked: true)
